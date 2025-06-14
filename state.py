59af21519a0a3aa4c38bf8e3a73106b927872d4d
AliayrSagandykov <161940265+AliayrSagandykov@users.noreply.github.com>
2025-06-14 19:55:40 +0600

    Delete state.py

--- state.py
+++
@@ -1,8 +0,0 @@
-from db import waiting_users, active_chats
-
-
-def is_user_waiting(user_id):
-    return user_id in waiting_users
-
-def is_user_in_chat(user_id):
-    return user_id in active_chats
